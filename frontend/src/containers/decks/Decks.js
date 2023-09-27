49a740e1e528feb6785a58d9eb78496f025ed7e8
happy_ampersand <poliakovegor888@gmail.com>
2023-09-28 00:47:12 +0800

    fix decks layout

--- frontend/src/containers/decks/Decks.js
+++ frontend/src/containers/decks/Decks.js
@@ -12,12 +12,51 @@ const decks = [
         title: 'some deck',
         difficulty: 'beginner'
     },
-    {   id: 2,
+    {   
+        id: 2,
         title: 'some deck',
         difficulty: 'beginner'
     },
     { 
         id: 3,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 4,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 5,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 6,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 7,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 8,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 9,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 10,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 11,
         title: 'some deck',
         difficulty: 'beginner'
     },
@@ -31,12 +70,51 @@ const decks = [
         title: 'some deck',
         difficulty: 'beginner'
     },
-    {   id: 2,
+    {   
+        id: 2,
         title: 'some deck',
         difficulty: 'beginner'
     },
     { 
         id: 3,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 4,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 5,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 6,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 7,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 8,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 9,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 10,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 11,
         title: 'some deck',
         difficulty: 'beginner'
     },
@@ -50,12 +128,51 @@ const decks = [
         title: 'some deck',
         difficulty: 'beginner'
     },
-    {   id: 2,
+    {   
+        id: 2,
         title: 'some deck',
         difficulty: 'beginner'
     },
     { 
         id: 3,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 4,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 5,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 6,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 7,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 8,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {
+        id: 9,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    {   id: 10,
+        title: 'some deck',
+        difficulty: 'beginner'
+    },
+    { 
+        id: 11,
         title: 'some deck',
         difficulty: 'beginner'
     }
@@ -66,8 +183,10 @@ function Decks() {
         return <Deck deckData={deck} key={deck.id} />
     });
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
-            {decksElement}
+        <div className='flex justify-center items-center col-start-2 col-end-10'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-5'>
+                {decksElement}
+            </div>
         </div>
     );
 }
