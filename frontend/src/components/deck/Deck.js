250dd1159fa67e33e543e92f8c2c0f683e6e5d48
happy_ampersand <poliakovegor888@gmail.com>
2023-10-15 17:48:26 +0800

    add play button to deck, fix deck link

--- frontend/src/components/deck/Deck.js
+++ frontend/src/components/deck/Deck.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import 'react-circular-progressbar/dist/styles.css';
 import DifficultyLabel from '../difficulty_label/DifficultyLabel';
 
 function Deck({deckData}) {
     const cardsLearned = deckData.cards_learned
     const cardsStudying = deckData.cards_studying
     const cardsOverall = cardsLearned + cardsStudying
+    const circumference = 30 * 2 * Math.PI;
+    const percent = 10;
+    const strokeOffset = circumference - percent / 100 * circumference;
     return (
         <div className='flex flex-col justify-between rounded-md bg-white shadow-md'>
             <div className='flex flex-col bg-gradient-to-b from-blue-500 to-blue-300 items-start rounded-t-md p-2'>
                 <p className='text-white text-xl font-semibold mb-3'>{deckData.title}</p>
                 <DifficultyLabel difficulty={deckData.difficulty}/>
             </div>
-            <div className='flex place-content-center justify-evenly p-2 w-64'>
+            <div className='flex items-center justify-evenly p-2 w-64'>
                 <div className='flex flex-col text-center'>
                     <div className='text-xl font-bold'>{cardsLearned}</div>
                     <div className='text-gray-400'>Learned</div>
@@ -21,7 +28,13 @@ function Deck({deckData}) {
                     <div className='text-xl font-bold'>{cardsStudying}</div>
                     <div className='text-gray-400'>Studying</div>
                 </div>
-                <Link className='flex items-center' to={`/study`} state={deckData.id}>Study</Link>
+                <div className='w-16'>
+                    <CircularProgressbarWithChildren value={percent}>
+                        <Link to='/study' state={deckData.id} className="absolute text-xl text-blue-700">
+                            <FontAwesomeIcon icon={faPlay} />
+                        </Link>
+                    </CircularProgressbarWithChildren>
+                </div>
             </div>
         </div>
     );
